61b3ff7544f5bc8e385b34dcbdf31f6b52756a3f
vyash5075 <vyash5075@gmail.com>
2019-12-15 19:51:09 +0530

    count leaf nodes

---
+++ trees/count_leaf.java
@@ -0,0 +1,73 @@
+import java.util.*;
+public class count_leaf {
+    static class node{
+        int data;
+        node left;
+        node right;
+        node (int d)
+        {
+            this.data=d;
+            left=null;
+            right=null;
+        }
+    }
+    static node root;
+    public static node insert(node root,int data)
+    {
+        node temp=new node(data);
+        if(root==null)
+        {
+            root=temp;
+            return root;
+        }
+        else{
+            if(data<root.data)
+            {
+                insert(root.left,data);
+                if(root.left==null)
+                {
+                    root.left=temp;
+                } 
+            }
+            else{
+                insert(root.right,data);
+                if(root.right==null)
+                {
+                    root.right=temp;
+                }
+            }
+        }
+        return root;
+    }
+   
+    static int count=0;
+    public static int countleaf(node root)
+    {
+        
+       if(root!=null)
+       {
+           if(root.left==null&&root.right==null)
+           {
+               count++;
+           }
+           else{
+               countleaf(root.left);
+               countleaf(root.right);
+           }
+       }
+       return count;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the no. of entries");
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+          root= insert(root,sc.nextInt());
+        }
+       
+        int a=countleaf(root);
+        System.out.println("leaf nodes="+ a);
+    }   
+}
