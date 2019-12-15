010e476dda6e2914ac748a5f823af7f177afb197
vyash5075 <vyash5075@gmail.com>
2019-12-15 19:36:41 +0530

    inorder

---
+++ trees/bst_inorder.java
@@ -0,0 +1,63 @@
+import java.util.*;
+public class bst_inorder {
+    static class node{
+        int data;
+        node left;
+        node right;
+        node (int d)
+        {
+            this.data=d;
+            left=null;
+            right=null;
+        }
+    }
+    static node root;
+    public static node insert(node root,int data)
+    {
+        node temp=new node(data);
+        if(root==null)
+        {
+            root=temp;
+            return root;
+        }
+        else{
+            if(data<root.data)
+            {
+                insert(root.left,data);
+                if(root.left==null)
+                {
+                    root.left=temp;
+                } 
+            }
+            else{
+                insert(root.right,data);
+                if(root.right==null)
+                {
+                    root.right=temp;
+                }
+            }
+        }
+        return root;
+    }
+    public static void inorder(node root)
+    {
+        if(root!=null)
+        {
+            inorder(root.left);
+            System.out.println(root.data);
+            inorder(root.right);
+        }
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the no. of entries");
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+          root= insert(root,sc.nextInt());
+        }
+        System.out.println("inorder");
+        inorder(root);
+    }   
+}
