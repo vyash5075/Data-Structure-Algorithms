5348e05cb1b5a59345e4d8dc054c9ba89944fcc3
Yash Verma <vyash5075@gmail.com>
2021-06-01 21:46:02 +0530

    Update Frequencysort.java

--- heaps/Frequencysort.java
+++ heaps/Frequencysort.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Heaps;
+package Heaps ;
 
 import java.util.ArrayList;
 import java.util.HashMap;
